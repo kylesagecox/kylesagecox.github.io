2759ee5112fc2ec67d521b8368da352cb9273b68
kylesagecox <112576848+kylesagecox@users.noreply.github.com>
2022-11-09 19:59:55 -0800

    Update external.js

--- external.js
+++ external.js
@@ -1,3 +1,6 @@
-if (document.querySelector('#about')) {
-	console.log('about');
+function scroll(target) {
+	let targetOffset = target.href.offsetTop;
+	console.log(targetOffset);
+	window.scrollTo({top: targetOffset, behavior: 'smooth'});
+	return false;
 }
