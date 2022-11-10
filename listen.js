176cfdb32d8ca66f1b4e1b578b01df24bd5d500d
kylesagecox <112576848+kylesagecox@users.noreply.github.com>
2022-11-09 20:30:16 -0800

    Update listen.js

--- listen.js
+++ listen.js
@@ -2,11 +2,11 @@ var hashLinks = document.querySelectorAll('a[href^="#"]');
 for (let item of hashLinks) {
 	item.addEventListener('click', (e)=> {
 		let hash = item.getAttribute('href');
-		let target = document.querySelector(hash)
+		let target = document.querySelector(hash);
 		target.scrollIntoView({
 			behavior: 'smooth',
 			block: 'start'
-		})
+		});
 	//	history.pushState(null, null, hash);
 		e.preventDefault();
 	})
