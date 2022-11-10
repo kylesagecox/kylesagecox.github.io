219e542862bb26c6ed9796b80559f95f75226e00
kylesagecox <112576848+kylesagecox@users.noreply.github.com>
2022-11-09 23:31:18 -0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -2,14 +2,12 @@ var hashLinks = document.querySelectorAll('a[href^="#"]');
 for (let item of hashLinks) {
 	item.addEventListener('click', (e)=> {
 		let hash = item.getAttribute('href');
-		console.log(hash);
 		if (hash === '#') {
 			window.scrollTo({top: 0, behavior: 'smooth'});
 		} else {
 			let target = document.querySelector(hash);
-			let targetTop = target.offsetTop; // target.getBoundingClientRect().top;
+			let targetTop = target.offsetTop;
 			let destination = targetTop - 80;
-			console.log(target, targetTop, destination);
 			window.scrollTo({top: destination, behavior: 'smooth'});
 		}
 		e.preventDefault();
